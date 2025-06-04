2c9a34fcf5d769f6e0ed46d3d26ec014daa48c33
InfoVac <infovac@somosicev.com>
2025-06-04 00:59:02 -0300

    Primeiro commit: Frontend InfoVac

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env': env
+    },
+    server: {
+      port: 3000,
+      strictPort: true,
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-router-dom', 'lucide-react']
+    }
+  };
+}); 
